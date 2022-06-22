a12241b3f8e89c0bc4272e16f111c6e0006f57ee
MonicaPiemontesi <105297815+MonicaPiemontesi@users.noreply.github.com>
2022-06-22 15:48:23 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+  //scrolltop
+
+function scrollToTop() {
+      window.scrollTo(0, 0);
+    }
+//cookie
+    function showCookie() {
+      let cookie = document.querySelector('#cookie');
+      cookie.style = "display:block;";
+    }
+
+    function hideCookie() {
+      let cookie = document.querySelector('#cookie');
+      cookie.style = "display:none;";
+    }
+
